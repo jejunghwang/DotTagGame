e39be80b1be77529c46441597b9233f3044ff0bd
minwoo jeong <minuj7908@gmail.com>
2025-05-07 17:46:14 +0900

    add login request packet and login response packet

--- WindowsFormsApp4/Packet/Class1.cs
+++ WindowsFormsApp4/Packet/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace Packets
+﻿using System.Text;
+
+namespace Packets
 {
 
     public enum PacketType
@@ -6,8 +8,9 @@
         move,
         chat,
         skill,
-        login,
+        loginRequest,
         status,
+        loginResponse
     }
 
     public interface Header
@@ -44,6 +47,66 @@
             };
 
 
+        }
+    }
+
+    public class LoginRequestPacket : Header
+    {
+        public PacketType Type => PacketType.loginRequest;
+
+        public string id, pw;
+        
+        public byte[] ToBytes()
+        {
+            byte[] buffer = new byte[9 + id.Length + pw.Length];
+            buffer[0] = (byte)Type;
+            byte[] idBytes = Encoding.UTF8.GetBytes(id);
+            byte[] pwBytes = Encoding.UTF8.GetBytes(pw);
+
+            BitConverter.GetBytes(idBytes.Length).CopyTo(buffer, 1);
+            idBytes.CopyTo(buffer, 5);
+
+            BitConverter.GetBytes(pwBytes.Length).CopyTo(buffer, 5 + idBytes.Length);
+            pwBytes.CopyTo(buffer, 9 + idBytes.Length);
+
+            return buffer;
+        }
+
+        public static LoginRequestPacket FromBytes(byte[] buffer)
+        {
+            int idLength = BitConverter.ToInt32(buffer, 1);
+            string id = Encoding.UTF8.GetString(buffer, 5, idLength);
+
+            int pwLength = BitConverter.ToInt32(buffer, 5 + idLength);
+            string pw = Encoding.UTF8.GetString(buffer, 9 + idLength, pwLength);
+
+            return new LoginRequestPacket
+            {
+                id = id,
+                pw = pw
+            };
+        }
+    }
+
+    public class LoginResponsePacket : Header
+    {
+        public PacketType Type => PacketType.loginResponse;
+
+        bool successLogin;
+        public byte[] ToBytes()
+        {
+            byte[] buffer = new byte[2];
+            buffer[0] = (byte)Type;
+            BitConverter.GetBytes(successLogin).CopyTo(buffer, 1);
+            return buffer;
+        }
+
+        public static LoginResponsePacket FromBytes(byte[] buffer)
+        {
+            return new LoginResponsePacket
+            {
+                successLogin = BitConverter.ToBoolean(buffer, 1)
+            };
         }
     }
 }
