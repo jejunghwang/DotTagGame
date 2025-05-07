e4a557e76e5a7432dec2fcefd37a8d8638ae57e3
minwoo jeong <minuj7908@gmail.com>
2025-05-07 17:02:26 +0900

    make class library for packets. add packet type, packet header, and move packet.

---
+++ WindowsFormsApp4/Packet/Class1.cs
@@ -0,0 +1,49 @@
+﻿namespace Packets
+{
+
+    public enum PacketType
+    {
+        move,
+        chat,
+        skill,
+        login,
+        status,
+    }
+
+    public interface Header
+    {
+        PacketType Type { get; }
+        byte[] ToBytes();
+    }
+
+    public class MovePacket : Header
+    {
+        public PacketType Type => PacketType.move;
+
+        public int playerId;
+        public float x, y;
+
+        public byte[] ToBytes()
+        {
+            byte[] buffer = new byte[13];
+            buffer[0] = (byte)Type;
+            BitConverter.GetBytes(playerId).CopyTo(buffer, 1);
+            BitConverter.GetBytes(x).CopyTo(buffer, 5);
+            BitConverter.GetBytes(y).CopyTo(buffer, 9);
+
+            return buffer;
+        }
+
+        public static MovePacket FromBytes(byte[] buffer)
+        {
+            return new MovePacket
+            {
+                playerId = BitConverter.ToInt32(buffer, 1),
+                x = BitConverter.ToSingle(buffer, 5),
+                y = BitConverter.ToSingle(buffer, 9)
+            };
+
+
+        }
+    }
+}
