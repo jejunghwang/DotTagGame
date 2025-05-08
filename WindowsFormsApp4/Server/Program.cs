390ee977e97c14c3a2f977f041ef8a3e144eb402
minwoo jeong <minuj7908@gmail.com>
2025-05-08 13:05:10 +0900

    make server

--- WindowsFormsApp4/Server/Program.cs
+++ WindowsFormsApp4/Server/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using Packets;
@@ -11,6 +13,34 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            TcpListener server = null;
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            int port = 9999;
+
+            try
+            {
+                server = new TcpListener(addr, port);
+                server.Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+
+                }
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                server.Stop();
+            }
+
+            Console.WriteLine("\n서버가 종료됩니다.");
         }
     }
 }
