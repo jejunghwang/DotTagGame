07dca68f8dbb395fe994e42b5c854d52e4753869
newyearsdayis <jeongeun9278@gmail.com>
2025-06-19 00:09:35 +0900

    add itemBox image

--- WindowsFormsApp4/WindowsFormsApp4/Properties/Resources.Designer.cs
+++ WindowsFormsApp4/WindowsFormsApp4/Properties/Resources.Designer.cs
@@ -240,6 +240,16 @@ namespace WindowsFormsApp4.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap itemBox {
+            get {
+                object obj = ResourceManager.GetObject("itemBox", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap itemFrame {
             get {
                 object obj = ResourceManager.GetObject("itemFrame", resourceCulture);
